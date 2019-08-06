e9fd312999b717d1645d4f1f8f4d69628c0ec4ec
g-barbosa <giovanne.bbarbosa@gmail.com>
2019-08-06 13:53:05 -0300

    add navbar

---
+++ src/components/Bars/Nav.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import {Link} from "react-router-dom"
+import logo from '../../assets/logo.png'
+import '../../css/Nav.css'
+
+export default class Nav extends Component {
+	render(){
+		return(
+			<div className="bg-white fixed-top shadow">
+				<div className="container">
+					<nav className="navbar navbar-expand-lg">
+						<img className='logoNav' src={logo}/>
+						<div className="collapse navbar-collapse">
+							<ul className="nav navbar-nav ml-auto">
+								<li >
+									<Link className="nav-link linkR" to="/foods">FOODS</Link>
+								</li>
+								<li>
+									<Link className="nav-link linkR" to="/people">PEOPLE</Link>
+								</li>
+								<li>
+									<Link className="nav-link linkR" to="/places">PLACES</Link>
+								</li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</div>
+
+		)
+	}
+}
